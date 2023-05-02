97fe9e044f577f16a368f811335d695e95ec0870
fofo <qqqeee@gmail.com>
2023-05-02 17:35:41 +0800

    fix

--- app.py
+++ app.py
@@ -7,14 +7,24 @@ import gradio as gr
 
 cache_dir = os.path.join(Path.cwd(), "cache")
 # stage 1
-stage_1 = DiffusionPipeline.from_pretrained("DeepFloyd/IF-I-XL-v1.0", variant="fp16", torch_dtype=torch.float16,
-                                            resume_download=True, )
+stage_1 = DiffusionPipeline.from_pretrained(
+    "DeepFloyd/IF-I-XL-v1.0",
+    variant="fp16",
+    torch_dtype=torch.float16,
+    resume_download=True,
+
+)
 # stage_1.enable_xformers_memory_efficient_attention()  # remove line if torch.__version__ >= 2.0.0
 stage_1.enable_model_cpu_offload()
 
 # stage 2
 stage_2 = DiffusionPipeline.from_pretrained(
-    "DeepFloyd/IF-II-L-v1.0", text_encoder=None, variant="fp16", torch_dtype=torch.float16, resume_download=True,
+    "DeepFloyd/IF-II-L-v1.0",
+    text_encoder=None,
+    variant="fp16",
+    torch_dtype=torch.float16,
+    resume_download=True,
+
 )
 # stage_2.enable_xformers_memory_efficient_attention()  # remove line if torch.__version__ >= 2.0.0
 stage_2.enable_model_cpu_offload()
@@ -22,8 +32,13 @@ stage_2.enable_model_cpu_offload()
 # stage 3
 safety_modules = {"feature_extractor": stage_1.feature_extractor, "safety_checker": stage_1.safety_checker,
                   "watermarker": stage_1.watermarker}
-stage_3 = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-x4-upscaler", **safety_modules,
-                                            torch_dtype=torch.float16, resume_download=True, )
+stage_3 = DiffusionPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-x4-upscaler",
+    **safety_modules,
+    torch_dtype=torch.float16,
+    resume_download=True,
+
+)
 # stage_3.enable_xformers_memory_efficient_attention()  # remove line if torch.__version__ >= 2.0.0
 stage_3.enable_model_cpu_offload()
 
@@ -47,7 +62,7 @@ def gen(prompt, negative_prompt):
     # pt_to_pil(image)[0].save("./if_stage_II.png")
 
     # stage 3
-    image3 = stage_3(prompt=prompt, image=image2, generator=generator, noise_level=100).images
+    image3 = stage_3(prompt=prompt, image=image2, generator=generator, noise_level=100, ).images
     # image3[0].save("./if_stage_III.png")
     return pt_to_pil(image1)[0], pt_to_pil(image2)[0], image3[0]
 
@@ -59,6 +74,7 @@ with gr.Blocks() as demo:
         show_label=False,
         lines=4,
         max_lines=6,
+        value="high quality dslr photo, a photo product of a lemon inspired by natural and organic materials, wooden accents, intricately decorated with glowing vines of led lights, inspired by baroque luxury",
         placeholder='Enter a negative prompt',
         elem_id='negative-prompt-text-input',
     )
@@ -70,7 +86,7 @@ with gr.Blocks() as demo:
             elem_id='gallery'
         )
 
-    gen_btn = gr.Button(lable="gen")
+    gen_btn = gr.Button()
     gen_btn.click(
         gen,
         inputs=[prompt, negative_prompt],
